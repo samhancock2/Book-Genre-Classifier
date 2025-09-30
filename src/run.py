fc4dd3bba930d5a3717cb70511f0623664c55d5d
samhancock2 <saha118118@gmail.com>
2025-09-30 17:55:56 +0100

    unique folder names for results

--- src/run.py
+++ src/run.py
@@ -9,7 +9,9 @@ from evaluate import evaluate
 with open("configs/config.yaml") as f:
     config = yaml.safe_load(f)
 
-# ---- Create a meaningful save directory with training params ----
+# ---- Create a meaningful save directory with training params and timestamp ----
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")  # e.g., 20250930_153012
+
 embedding_type = config['dataset'].get('embedding_type', 'glove')
 epochs = config['training']['epochs']
 batch_size = config['training']['batch_size']
@@ -17,12 +19,12 @@ lr = config['training']['lr']
 embedding_dim = config['embedding']['dim']
 
 if embedding_type == 'glove':
-    save_dir_name = f"glove_{embedding_dim}e{epochs}bs{batch_size}lr{lr}"
+    save_dir_name = f"glove_{embedding_dim}e{epochs}bs{batch_size}lr{lr}t{timestamp}"
 elif embedding_type == 'sentence_transformers':
     model_name = config['dataset'].get('transformer_model', 'unknown_model').replace("/", "_")
-    save_dir_name = f"transformer_{model_name}_e{epochs}bs{batch_size}lr{lr}"
+    save_dir_name = f"transformer_{model_name}_e{epochs}bs{batch_size}lr{lr}t{timestamp}"
 else:
-    save_dir_name = f"experiment_e{epochs}bs{batch_size}lr{lr}"
+    save_dir_name = f"experiment_e{epochs}bs{batch_size}lr{lr}t{timestamp}"
 
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 save_dir = os.path.join(project_root, "results", save_dir_name)
